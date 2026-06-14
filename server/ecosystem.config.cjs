8027f01d15c8103fb91fa64c15f2046488558ed5
Tarlan Nuriyev <hi@tarlannuriyev.com>
2026-06-14 21:08:10 +0400

    fix: ecosystem config CommonJS syntax

--- server/ecosystem.config.cjs
+++ server/ecosystem.config.cjs
@@ -1,10 +1,10 @@
-{
-  "apps": [{
-    "name": "analytics-govix",
-    "script": "index.js",
-    "cwd": "/root/analytics.govix.io/server",
-    "env": { "PORT": "3030", "NODE_ENV": "production" },
-    "restart_delay": 3000,
-    "max_restarts": 10
+module.exports = {
+  apps: [{
+    name: 'analytics-govix',
+    script: 'index.js',
+    cwd: '/root/analytics.govix.io/server',
+    env: { PORT: '3030', NODE_ENV: 'production' },
+    restart_delay: 3000,
+    max_restarts: 10
   }]
-}
+};
